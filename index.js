29eebaf13196b4139f9157ab0e5b21936b553b00
Samuel Montalvo <sammontalvojr@gmail.com>
2020-05-26 15:16:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ let takeANumber = (currentLine, name) => {
   currentLine.push(name);
   return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
 }
+
+let nowServing = (currentLine) => {
+  
+}

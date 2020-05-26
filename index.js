e55a2c83eb98789924a44dedaa66de9d59af77eb
Samuel Montalvo <sammontalvojr@gmail.com>
2020-05-26 15:20:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-let takeANumber = (currentLine, name) => {
+const takeANumber = (currentLine, name) => {
   currentLine.push(name);
   return `Welcome, ${name}. You are number ${currentLine.length} in line.`;
 }
 
-let nowServing = (currentLine) => {
+const nowServing = (currentLine) => {
   if(currentLine.length === 0) {
     return 'There is nobody waiting to be served!';
-  } else {
-    
   }
+  let name = currentLine[0];
+  
 }
